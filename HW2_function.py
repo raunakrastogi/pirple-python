6dbc8b7ec88fe69550cb74f9acd245f043516912
Raunak Rastogi <rastograunak@gmail.com>
2021-06-30 17:11:16 +0530

    Update and rename func.py to HW2_function.py

---
+++ HW2_function.py
@@ -0,0 +1,31 @@
+# Homework Assignment 2
+"""
+Let's return to the music example from assignment one. Pick 3 of the attributes you listed. For our example we're going to say "Genre", "Artist" and "Year". Create a new Python file and create 3 functions with the same name as those attributes. So in this example we'd have one function named "genre" another named "artist" and another called "year".
+
+When someone calls any of those functions, that function should return the corresponding value for that attribute. For example, if we call the "Artist" function our function would return "Dave Brubeck". Yours should return whatever values make sense for your music choice.
+"""
+"""
+For this Assignment, I have chosen Name, AlbumName and VideoViews as the three attributes.
+
+"""
+name = "Ride it"
+albumName = "My Own way"
+videoViews = 13.7
+
+# Global Variables are declared
+
+def Name():
+    print(name)
+
+def AlbumName():
+    print(albumName)
+
+def VideoViews():
+    print(videoViews)
+
+# Now i have called all the functions one by one
+Name()
+AlbumName()
+VideoViews()
+
+# Coudn't understand what to exactly do with the boolean data type.

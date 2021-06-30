b49071eaa111ada2acda2d299310f9300913a998
Raunak Rastogi <rastograunak@gmail.com>
2021-06-30 17:12:48 +0530

    Update and rename main.py to HW1_variables.py

---
+++ HW1_variables.py
@@ -0,0 +1,71 @@
+# Homework Assignment 1.
+"""
+What's your favorite song?
+
+Think of all the attributes that you could use to describe that song. That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many different characteristics as you can.
+
+In your text editor, create an empty file and name it main.py
+
+Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, and giving each variable a value. Here's an example:
+
+```
+Genre = "Jazz"
+
+```
+
+Give each variable its own line. Then, after you have listed the variables, print each one of them out.
+
+ For example:
+
+```
+
+Artist = "Dave Brubeck"
+Genre = "Jazz"
+DurationInSeconds = 328
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+
+
+```
+Your actual assignment should be significantly longer than the example above. Think of as many characteristics of the song as you can. Try to use Strings,  Integers and Decimals (floats)!
+"""
+Name = "Ride It"
+# There is an Indian Version of this song which makes it so special for me.  
+ArtistName = "Jay Sean"
+# I adore him as he is part Indian.
+Genre = "R&B/Soul"
+LikesOnYTMusic = 304
+# Highly Underrated in my opinion to be honest.
+AlbumName = "My Own Way"
+ReleaseDate = "12 May, 2008"
+DurationInMins = 3.10
+# 3 Minutes and 10 seconds of greatness.
+AlbumPosition = "The album peaked at number six on the UK Albums Chart"
+WrittenBy = "Jay Sean and Alan Sampson"
+ProducedBy = "Alan Sampson"
+VideoViews = 13.7
+# 13.7 Million Views on the Youtube Video of this Song.
+Label = "Jayded, 2Point9"
+Certification = "Silver"
+# BPI Certification.
+RemixBy = "DJ Regard"
+# Released in 2019, it is a worldwide TikTok Hit with millions of daily listeners.
+
+print(Name)
+print(ArtistName)
+print(Genre)
+print(LikesOnYTMusic)
+print(AlbumName)
+print(ReleaseDate)
+print(DurationInMins)
+print(AlbumPosition)
+print(WrittenBy)
+print(ProducedBy)
+print(VideoViews)
+print(Label)
+print(Certification)
+print(RemixBy)
+
+# All the details are taken from the internet through various sources.
